3972b0b4d7086521c12f7ba436002353aeb5f9d4
nouha404 <aboubacar.marega@ism.edu.sn>
2023-12-05 18:45:38 +0100

    Filtring manga

--- app/manga/urls.py
+++ app/manga/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, MangaView, ApiView, MangaDetailView
+from .views import HomeView, MangaView, ApiView, MangaDetailView, MangaSearchView
 
 from django.views import View
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", HomeView.as_view(), name="manga-index"),
     path("manga/", MangaView.as_view(), name="manga-view"),
     path("manga/<int:pk>/", MangaDetailView.as_view(), name="manga-details"),
-    # path('search/', MangaSearchView.as_view(), name='search-manga'),
     path('api/', ApiView.as_view(), name="api-view"),
+    path("manga/only/", MangaSearchView.as_view(), name="manga-filtre"),
 ]
--- app/manga/views.py
+++ app/manga/views.py
@@ -1,7 +1,10 @@
-
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.views.generic import TemplateView, ListView, DetailView
 
 from scrapping.models import Informations
+from .forms import SearchMangaForm
 
 from django.views import View
 
@@ -19,6 +22,13 @@ class MangaView(ListView):
     template_name = "manga/mangas.html"
     context_object_name = "informations"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = SearchMangaForm()
+        return context
+
+
+
 
 class ApiView(TemplateView):
     template_name = "manga/mainApi.html"
@@ -29,9 +39,43 @@ class MangaDetailView(DetailView):
     template_name = "manga/details.html"
     context_object_name = "info"
 
+    # un autre contexte
+
+
 
 """"
 class MangaSearchView(View):
     La partie recherche
     template_name = 'manga/results.html'
 """
+
+
+def searchManga(request):
+   pass
+
+
+class MangaSearchView(View):
+    template_name = 'manga/mangas.html'
+    template_for_resultat = 'manga/search.html'
+
+    # Traitement du get/post du filtre
+    def post(self, request, *args, **kwargs):
+        form = SearchMangaForm(request.POST)
+        if form.is_valid():
+            search_term = form.cleaned_data['search']
+
+            # Filtrer les informations basées sur le terme de recherche
+            results = Informations.objects.filter(manga_title__icontains=search_term)
+            context = {'informations': results, 'form': form}
+            if results:
+                if request.is_ajax():
+
+                    html_content = render_to_string(self.template_for_resultat, context)
+                    return JsonResponse({'html': html_content})
+                else:
+                    return render(request, self.template_for_resultat, context)
+
+                #return render(request, self.template_for_resultat, context)
+
+        context = {'form': form}
+        return render(request, self.template_name, context)
